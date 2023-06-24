c4f93322e67f2a993a96378c9e11f04e2e9bfbb0
Ubuntu <ubuntu@ip-172-31-35-80.us-east-2.compute.internal>
2023-06-24 12:33:16 +0000

    security group and key-pair added

---
+++ Ec2.tf
@@ -0,0 +1,28 @@
+  = "us-east-2"
+}
+
+# Create EC2 instance
+resource "aws_instance" "jump-box" {
+  ami                    = "ami-024e6efaf93d85776"
+  instance_type          = "t2.micro"
+  key_name               = "Devops-key2"
+  vpc_security_group_ids = [aws_security_group.Project_1_sg.id]
+
+  tags = {
+    Name        = "Project1_EC2"
+    name        = "jumphost"
+    provisioner = "Terraform"
+  }
+}
+
+#Create security group
+
+resource "aws_security_group" "Project_1_sg" {
+  name   = "terraform created"
+  
+    ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
