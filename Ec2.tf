2877acd426adb0550fdf5e68f8a8db8c8f7fe64b
root <root@ip-172-31-35-80.us-east-2.compute.internal>
2023-06-24 14:47:19 +0000

    sec gr separated from Ec2

--- Ec2.tf
+++ Ec2.tf
@@ -18,19 +18,3 @@ resource "aws_instance" "jump-box" {
 
 #Create security group
 
-resource "aws_security_group" "Project_1_sg" {
-  name   = "terraform created"
-  
-    ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-}
-}
---
+++ security_group.tf
@@ -0,0 +1,18 @@
+#Create security group
+
+resource "aws_security_group" "Project_1_sg" {
+    name   = "terraform created"
+  
+    ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+}
