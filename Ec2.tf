3bf3bd5add85f3b8b9a69b4693804a53274d91fb
Dele <oyedeloy@gmail.com>
2023-06-24 15:50:23 +0000

    \security group and ec2 merged

--- Ec2.tf
+++ Ec2.tf
@@ -2,12 +2,29 @@ provider "aws" {
     region  = "us-east-2"
 }
 
+
+resource "aws_security_group" "Simpli_EC_proj_1" {
+    name   = "terraform created"
+  
+    ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+}
 # Create EC2 instance
 resource "aws_instance" "jump-box" {
   ami                    = "ami-024e6efaf93d85776"
   instance_type          = "t2.micro"
   key_name               = "devops-key2"
-  vpc_security_group_ids = [sg-04bc51110db00083d]
+  vpc_security_group_ids = [aws_security_group.Simpli_EC_proj_1.id]
 
   tags = {
     Name        = "Project1_EC2"
@@ -16,5 +33,4 @@ resource "aws_instance" "jump-box" {
   }
 }
 
-#Create security group
 
