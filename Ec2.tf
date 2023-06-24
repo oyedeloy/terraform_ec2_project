57f83c97e296e073f0d66fa8baa2b6c99297fa16
root <root@ip-172-31-35-80.us-east-2.compute.internal>
2023-06-24 15:40:53 +0000

    fixing errors in security group

--- Ec2.tf
+++ Ec2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "jump-box" {
   ami                    = "ami-024e6efaf93d85776"
   instance_type          = "t2.micro"
   key_name               = "devops-key2"
-  vpc_security_group_ids = [aws_security_group.Project_1_sg2.id]
+  vpc_security_group_ids = [sg-04bc51110db00083d]
 
   tags = {
     Name        = "Project1_EC2"
